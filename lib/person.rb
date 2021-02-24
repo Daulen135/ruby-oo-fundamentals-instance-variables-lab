f6111da2d6b201230e4dab33a0bcf42bd50c3578
Daulen135 <daulenz@gmail.com>
2021-02-23 23:41:28 -0600

    commit tree 7ea9478dfabbec8f3e9b0f2b6ca4e36802a36c9a

---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+end
+# Under your Person class definition, create two people in local variables, adele_goldberg and alan_kay
+adele_goldberg=Person.new
+adele_goldberg.name="adele_goldberg"
+
+
+
+alan_kay=Person.new
+alan_kay.name="alan_kay"
+
